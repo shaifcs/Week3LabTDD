a4f412c16587164a871b810aa1b95dde92a6a5a0
shaiful <you@example.com>
2019-02-04 22:18:37 -0700

    The basic program

---
+++ src/main/java/cmput402/tdd/Utility.java
@@ -0,0 +1,19 @@
+package cmput402.tdd;
+
+import java.util.HashMap;
+
+import cmput402.tdd.service.Search;
+
+public class Utility {
+
+	public HashMap<String, Integer>  countAverageScore(Search search){
+		
+		return null;
+	}
+	
+public HashMap<String, Integer>  findTopScorer(Search search){
+		
+		return null;
+	}
+	
+}
